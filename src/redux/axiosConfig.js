71a138900812e53e100ad63db6d648538c34202a
InfoLazeTech <deepakinf30@gmail.com>
2025-07-23 17:19:17 +0530

    axios change

--- src/redux/axiosConfig.js
+++ src/redux/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosConfig = axios.create({
     // baseURL: 'http://localhost:3001/api/',
-    baseURL:'https://gantt-tracker-backend.vercel.app/',
+    baseURL:'https://gantt-tracker-backend.vercel.app/api/',
     headers: {
       'Content-Type': 'application/json',
     },
